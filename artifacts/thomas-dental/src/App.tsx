3e5a1789e0166cb165cb945fd0f63a164510d194
rushingfire9699 <cjthomasdmd@gmail.com>
2026-09-09 23:06:45 -0400

    Reset scroll position on route changes

--- artifacts/thomas-dental/src/App.tsx
+++ artifacts/thomas-dental/src/App.tsx
@@ -1,4 +1,5 @@
-import { Switch, Route } from "wouter";
+import { useEffect } from "react";
+import { Switch, Route, useLocation } from "wouter";
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
 import Home from "@/pages/home";
@@ -10,9 +11,20 @@ import PrivacyPage from "@/pages/privacy";
 import AccessibilityPage from "@/pages/accessibility";
 import NotFoundPage from "@/pages/not-found";
 
+function ScrollToTop() {
+  const [location] = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return null;
+}
+
 export default function App() {
   return (
     <div className="flex flex-col min-h-screen">
+      <ScrollToTop />
       <Navigation />
       <main className="flex-grow">
         <Switch>
